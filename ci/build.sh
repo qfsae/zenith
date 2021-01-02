ce51c0f78042a601fbb7b4866f90ea82403cd405
ethan <17emp5@queensu.ca>
2021-01-02 00:29:02 -0500

    add duplicate removal in build script

--- ci/build.sh
+++ ci/build.sh
@@ -2,15 +2,25 @@
 
 echo "Starting PIO builds"
 
-# BR=`git branch --show-current` 
+BR=`git branch --show-current` 
 
+array=()
 # iterate through all the changed files between PR branch and master
 # if a directory that has been modified contains a platformio ini file,
 # then run a build and ensure that it still compiles.
-for i in `git diff --name-only master`
+for i in `git diff --name-only master $BR`
 do
     directory=`dirname $i`
     file="$directory/platformio.ini"
     # Check if the directory has a platformio.ini file
-    if test -e $file; then echo $file; fi
+    # NOTE: This currently finds the right paths but still has repitions
+    # must strip list of presenting same directory twice
+    if test -e $file; then array+=($file); fi
+done
+
+finalList=($(printf "%s\n" "${array[@]}" | sort -u))
+
+for value in "${finalList[@]}"
+do
+     echo $value
 done
