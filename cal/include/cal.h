1cc9262da03df0e374a718529e88688fc1240036
Ethan Peterson <ethan@petetech.net>
2021-05-28 18:44:56 -0400

    setup CAL base class and pull into steering pio project

---
+++ cal/include/cal.h
@@ -0,0 +1,34 @@
+#ifndef CAL_H
+#define CAL_H
+
+#include <cstdint>
+
+struct CALMessage {
+    uint32_t id;
+    uint8_t data[];
+};
+
+class CAL {
+    public:
+        virtual bool init() = 0;
+        virtual CALMessage receive() = 0;
+        virtual bool send(CALMessage) = 0;
+
+        // Called on message receive if the underlying hardware has
+        // Rx interrupt
+        void setReceiveHandler(void (*handle)()) {
+            receiveHandler = handle;
+        }
+
+        // Called on transmit complete if underlying hardware has
+        // Tx interrupt
+        void setSendHandler(void (*handle)()) {
+            sendHandler = handle;
+        }
+        
+    private:
+        void (*receiveHandler)();
+        void (*sendHandler)();
+};
+
+#endif
