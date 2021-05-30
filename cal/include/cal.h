ae9e7cc124ce5ee5cace910188e017b5059b0d23
Ethan Peterson <ethan@petetech.net>
2021-05-30 13:10:14 -0400

    comment line break fixup

--- cal/include/cal.h
+++ cal/include/cal.h
@@ -14,14 +14,12 @@ class CAL {
         virtual CALMessage receive() = 0;
         virtual bool send(CALMessage) = 0;
 
-        // Called on message receive if the underlying hardware has
-        // Rx interrupt
+        // Called on message receive if the underlying hardware has Rx interrupt
         void setReceiveHandler(void (*handle)()) {
             receiveHandler = handle;
         }
 
-        // Called on transmit complete if underlying hardware has
-        // Tx interrupt
+        // Called on transmit complete if underlying hardware has Tx interrupt
         void setSendHandler(void (*handle)()) {
             sendHandler = handle;
         }
