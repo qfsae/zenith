42e0930f0fc4d2bda874a35020ed0b1b9b2d9ff8
Logan Lim <17LL43@queensu.ca>
2022-11-17 20:14:38 -0500

    Steering Display Template (#43)
    * FT800-FT813 library example
    
    * Template project w/ splash screen
    
    * Add readme and update splash screen
    
    * update backlight hz for scan line fix
    
    * address comments

---
+++ templates/steering-display/src/tft.h
@@ -0,0 +1,21 @@
+
+#ifndef TFT_H
+#define TFT_H
+
+// Debug Stats
+extern uint16_t num_profile_a, num_profile_b;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void TFT_init(void);
+void TFT_touch(void);
+void TFT_display(void);
+void TFT_splash(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TFT_H */
