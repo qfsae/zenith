2bf25e74a268476dc59422bfa6be5c3fc3b16420
Ethan Peterson <17emp5@queensu.ca>
2022-11-27 17:32:44 -0500

    ini file clean

--- templates/steering-display/src/screens.c
+++ templates/steering-display/src/screens.c
@@ -5,7 +5,6 @@
 // add functions or EVE calls to this file to run them onscreen
 // Put UI specific code here to decouple it from the tft library Logan set up
 // Note that we cannot run these in main.cpp due to name mangling.
-
 // Keep all the logic of switching between displays in main.cpp
 
 void display_code() {
