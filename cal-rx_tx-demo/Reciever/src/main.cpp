a289f566b4d8a25938f809dfb5e30203c67ad600
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2023-01-14 16:12:08 -0500

    library imported + compiles {untested}
    Everything appears to work, but can't test until encoder side is up and running

--- cal-rx_tx-demo/Reciever/src/main.cpp
+++ cal-rx_tx-demo/Reciever/src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
+#include <cal.hpp>
+#include "mcp2515_can.h"
+#include <SPI.h>
+
+mcp2515_can can(9);
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while(can.begin(CAN_1000KBPS) != CAN_OK){
+    Serial.println("CAN Init Error..");
+    delay(100);
+  }
+  Serial.println("CAN Bus Initialized");
 }
 
+int throttlePosition;
+int engineRPM = 0;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(can.checkReceive() == CAN_MSGAVAIL){
+    CAL::CAN_msg_t can_recv;
+    can.readMsgBuf(&can_recv.len, can_recv.data);
+    can_recv.id = can.getCanId();
+    CAL::update(can_recv, CAL::DATA_ECU::ThrottlePosition, &throttlePosition);
+    CAL::update(can_recv, CAL::DATA_ECU::EngineRPM, &engineRPM);
+  }
+  Serial.println(String("Throttle Position: ") + throttlePosition + String("\tEngine RPM: ") + engineRPM);
+  delay(100);
 }
