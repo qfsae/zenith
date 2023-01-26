8a13ff4f5d55a73005c566e540f21c4c5d5cdb35
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2023-01-25 19:21:29 -0500

    create
    start with base (ethan-ece-night)

---
+++ Q22Steering/src/tft.h
@@ -0,0 +1,30 @@
+#ifndef TFT_H
+#define TFT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Quick hack to extern the CAL data over to tft.c
+typedef struct {
+    int rpm;
+    int speed;
+    int engine_temp;
+    int coolant_temp;
+    int tps;
+    bool status;
+    int gear_pos;
+} DataHolder;
+
+void TFT_init(void);
+void TFT_touch(void);
+void TFT_display(void);
+void TFT_splash(void);
+
+extern DataHolder ecu_data;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TFT_H */
