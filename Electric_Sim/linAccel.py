0cd670988fe045447fb76ce13f9e37d90c528f2d
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2023-06-14 20:43:55 -0700

    enter motor constants

---
+++ Electric_Sim/linAccel.py
@@ -0,0 +1,9 @@
+import matplotlib
+import math
+
+def main():
+    accel = 0
+    torque = 0
+    return 0
+
+main()
---
+++ Electric_Sim/motorInfo.py
@@ -0,0 +1,16 @@
+# Maximum Peak Torque (Nm)
+torqueBurst   = int(150)
+# Continuous Torque (Nm)
+torqueConn    = int(80)
+# Torque  / Current Draw (Nm/Arms)
+torqueCurrent = float(.83)
+# Specific Load Speed (RPM/Vdc)
+rpmVoltage    = int(13)
+# Max RPM under load
+RPMmax_load   = int(5170)
+# Max RPM under no load
+RPMmax_noload = int(7050)
+# Continuous Motor Current
+CurrentConn   = int(100)
+# Burst Motor Current
+CurrentBurst  = int(200)
