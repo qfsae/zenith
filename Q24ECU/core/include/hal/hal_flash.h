13d0148f846fb51424dfbedd3beeedca5103f8d3
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2023-11-10 23:29:21 -0500

    Modifying flash works

---
+++ Q24ECU/core/include/hal/hal_flash.h
@@ -0,0 +1,138 @@
+/**
+ * @file hal_flash.h
+ * @author Jacob Chisholm (Jchisholm204.github.io)
+ * @brief Base Abstraction for interaction with STM32F4xx Flash Memory
+ * @version 0.1
+ * @date 2023-11-10
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#pragma once
+#include "hal/hal_gpio.h"
+
+/** @defgroup FLASH_Program_Parallelism FLASH Program Parallelism
+  * @{
+  */
+#define FLASH_PSIZE_BYTE           0x00000000U
+#define FLASH_PSIZE_HALF_WORD      0x00000100U
+#define FLASH_PSIZE_WORD           0x00000200U
+#define FLASH_PSIZE_DOUBLE_WORD    0x00000300U
+#define CR_PSIZE_MASK              0xFFFFFCFFU
+
+/** @defgroup FLASH_Keys FLASH Keys
+  * Taken from STM32 HAL
+  */ 
+#define RDP_KEY                  ((uint16_t)0x00A5)
+#define FLASH_KEY1               0x45670123U
+#define FLASH_KEY2               0xCDEF89ABU
+#define FLASH_OPT_KEY1           0x08192A3BU
+#define FLASH_OPT_KEY2           0x4C5D6E7FU
+
+// User Flash Start defined in linker script
+#define FLASH_USER_START 0x08060000
+
+// Flash write successful
+#define FLASH_WRITE_OK 0
+// Invalid Flash Address given
+#define FLASH_WRITE_ADDR_INVALID 1
+// Failed to assert write permissions on the flash memory
+#define FLASH_WRITE_NOACC 1
+// Data Requested does not exist
+#define FLASH_WRITE_NODAT 2
+
+
+static void hal_FLASH_Lock(void)
+{
+  FLASH->CR |= FLASH_CR_LOCK;
+};
+
+
+/**
+ * @brief Write a uint32 to the flash
+ * 
+ * @param Address 
+ * @param data 
+ * @return uint8_t 
+ */
+static uint8_t hal_FLASH_WriteFB(uint32_t Address, uint32_t data1, uint32_t data2){
+    // Check if write address is valid
+    if(Address < FLASH_USER_START){
+        return FLASH_WRITE_ADDR_INVALID;
+    }
+
+    // Unlock Flash in preparation for write
+    if(((FLASH->CR) & (FLASH_CR_LOCK)) != 0) {
+
+    /* Authorize the FLASH Registers access */
+    FLASH->KEYR = FLASH_KEY1; // Keys taken from STM32 HAL
+    FLASH->KEYR = FLASH_KEY2;
+
+    /* Verify Flash is unlocked */
+        if((FLASH->CR & FLASH_CR_LOCK) != 0) {
+            return FLASH_WRITE_NOACC;
+        }
+    }
+
+
+    // Configure flash register to receive data
+    ((FLASH->CR) &= ~(FLASH_CR_PSIZE));
+    FLASH->CR |= FLASH_PSIZE_DOUBLE_WORD;
+    FLASH->CR |= FLASH_CR_PG;
+
+    // Write Data (first half)
+    *(__IO uint32_t*)Address = data1;
+
+  /* Barrier to ensure programming is performed in 2 steps, in right order
+    (independently of compiler optimization behavior) */
+    __ASM volatile ("isb 0xF":::"memory");
+
+    // write data (second half)
+    *(__IO uint32_t*)(Address+4) = data2;
+
+    // Finish by locking the flash
+    hal_FLASH_Lock();
+
+    // Return OK
+    return FLASH_WRITE_OK;
+}
+
+static uint8_t hal_FLASH_WriteHB(uint32_t Address, uint32_t data){
+    // Check if write address is valid
+    if(Address < FLASH_USER_START){
+        return FLASH_WRITE_ADDR_INVALID;
+    }
+
+    // Unlock Flash in preparation for write
+    if(((FLASH->CR) & (FLASH_CR_LOCK)) != 0) {
+
+    /* Authorize the FLASH Registers access */
+    FLASH->KEYR = FLASH_KEY1; // Keys taken from STM32 HAL
+    FLASH->KEYR = FLASH_KEY2;
+
+    /* Verify Flash is unlocked */
+        if((FLASH->CR & FLASH_CR_LOCK) != 0) {
+            return FLASH_WRITE_NOACC;
+        }
+    }
+
+
+    // Configure flash register to receive data
+    ((FLASH->CR) &= ~(FLASH_CR_PSIZE));
+    FLASH->CR |= FLASH_PSIZE_DOUBLE_WORD;
+    FLASH->CR |= FLASH_CR_PG;
+
+    // Write Data
+    *(__IO uint32_t*)Address = data1;
+
+    // Finish by locking the flash
+    hal_FLASH_Lock();
+
+    // Return OK
+    return FLASH_WRITE_OK;
+}
+
+static const volatile uint32_t *hal_FLASH_Read(uint32_t Address){
+    return ((volatile uint32_t*)Address);
+}
