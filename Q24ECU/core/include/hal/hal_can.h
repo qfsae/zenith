3d234d85f4a15e513a57299b3d4d6f8c6084c8e5
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2023-12-17 13:32:00 -0500

    wip
    initialization not working

--- Q24ECU/core/include/hal/hal_can.h
+++ Q24ECU/core/include/hal/hal_can.h
@@ -13,6 +13,10 @@
 
 #include "stm32f4xx.h"
 #include "hal_gpio.h"
+#include "pins.h"
+
+#define CAN_EXT_ID_MASK                 0x1FFFFFFFU
+#define CAN_STD_ID_MASK                 0x000007FFU
 
 typedef struct {
     uint32_t id;        /* 29 bit identifier                               */
@@ -34,13 +38,92 @@ typedef enum {DATA_FRAME = 0, REMOTE_FRAME}         CAN_FRAME;
 
 typedef const struct
 {
-  uint8_t TS2;
-  uint8_t TS1;
-  uint8_t BRP;
+    uint8_t TS2;
+    uint8_t TS1;
+    uint8_t BRP;
 } CAN_bit_timing_config_t;
 
-CAN_bit_timing_config_t can_configs[6] = {{2, 12, 60}, {2, 12, 30}, {2, 12, 24}, {2, 12, 12}, {2, 12, 6}, {1, 7, 5}};
 
-static inline void hal_can_init(CAN_TypeDef * can ){
+static inline void hal_can_init(CAN_TypeDef * CAN, BITRATE bitrate){
+    CAN_bit_timing_config_t can_configs[6] = {{2, 12, 60}, {2, 12, 30}, {2, 12, 24}, {2, 12, 12}, {2, 12, 6}, {1, 7, 5}};
+    if(CAN == CAN1){
+        RCC->APB1ENR |= RCC_APB1ENR_CAN1EN;
+    }else if(CAN == CAN2){
+        RCC->APB1ENR |= RCC_APB1ENR_CAN2EN;
+    }
 
+    // Request initialization
+    SET_BIT(CAN->MCR, CAN_MCR_INRQ);
+    while((CAN->MSR & CAN_MSR_INAK) == 0U) asm("nop");
+
+    // Exit from sleep mode
+    CLEAR_BIT(CAN->MCR, CAN_MCR_SLEEP);
+    while((CAN->MSR & CAN_MSR_SLAK) != 0U) asm("nop");
+
+    // Disable time triggered communication mode
+    CLEAR_BIT(CAN->MCR, CAN_MCR_TTCM);
+    // Disable automatic bus-off management
+    CLEAR_BIT(CAN->MCR, CAN_MCR_ABOM);
+    // Disable automatic wakeup mode
+    CLEAR_BIT(CAN->MCR, CAN_MCR_AWUM);
+
+    // Enable Automatic retransmission based on Configuration File Defines
+    #ifdef USE_CAN_AUTOMATIC_RETRANSMISSION
+    CLEAR_BIT(CAN->MCR, CAN_MCR_NART);
+    #else
+    SET_BIT(CAN->MCR, CAN_MCR_NART);
+    #endif
+
+    // Disable receive FIFO locked mode
+    CLEAR_BIT(CAN->MCR, CAN_MCR_RFLM);
+    // Disable transmit FIFO priority
+    CLEAR_BIT(CAN->MCR, CAN_MCR_TXFP);
+    // Set bit timing
+    CAN->BTR &= ~(((0x03) << 24) | ((0x07) << 20) | ((0x0F) << 16) | (0x1FF)); 
+    CAN->BTR |=  (((can_configs[bitrate].TS2-1) & 0x07) << 20) | (((can_configs[bitrate].TS1-1) & 0x0F) << 16) | ((can_configs[bitrate].BRP-1) & 0x1FF);
+
+    // Leave initialization
+    CLEAR_BIT(CAN->MCR, CAN_MCR_INRQ);
+    while((CAN->MSR & CAN_MSR_INAK) != 0U) asm("nop");
+}
+
+static inline void hal_can_receive(CAN_TypeDef * CAN, CAN_msg_t * rx_msg){
+    // Determine Message ID format
+    uint32_t id = CAN->sFIFOMailBox[0].RIR;
+    if((id & (1U << 2U)) == 0){
+        rx_msg->format = STANDARD_FORMAT;
+        rx_msg->id = (CAN_STD_ID_MASK & (id >> 21U));
+    }
+    else{
+        rx_msg->format = EXTENDED_FORMAT;
+        rx_msg->id = (CAN_EXT_ID_MASK & (id >> 3U));
+    }
+
+    if((id & (1U << 1U)) == 0){
+        rx_msg->type = DATA_FRAME;
+    }
+    else{
+        rx_msg->type = REMOTE_FRAME;
+    }
+
+    // Message data length
+    rx_msg->len = (CAN->sFIFOMailBox[0].RDTR) & 0xFUL;
+
+    rx_msg->data[0] = 0xFFUL &  CAN->sFIFOMailBox[0].RDLR;
+    rx_msg->data[1] = 0xFFUL & (CAN->sFIFOMailBox[0].RDLR >> 8);
+    rx_msg->data[2] = 0xFFUL & (CAN->sFIFOMailBox[0].RDLR >> 16);
+    rx_msg->data[3] = 0xFFUL & (CAN->sFIFOMailBox[0].RDLR >> 24);
+    rx_msg->data[4] = 0xFFUL &  CAN->sFIFOMailBox[0].RDHR;
+    rx_msg->data[5] = 0xFFUL & (CAN->sFIFOMailBox[0].RDHR >> 8);
+    rx_msg->data[6] = 0xFFUL & (CAN->sFIFOMailBox[0].RDHR >> 16);
+    rx_msg->data[7] = 0xFFUL & (CAN->sFIFOMailBox[0].RDHR >> 24);
+
+    CAN->RF0R |= CAN_RF0R_RFOM0;
+    
+}
+
+static inline bool CANMsgAvail(CAN_TypeDef * CAN)
+{
+    // Check for pending FIFO 0 messages
+    return CAN->RF0R & 0x3UL;
 }
--- Q24ECU/core/src/tasks/testTasks.c
+++ Q24ECU/core/src/tasks/testTasks.c
@@ -14,15 +14,22 @@
 #include <stdio.h>
 #include "FreeRTOS.h"
 #include "task.h"
+#include "hal/hal_can.h"
 
 long unsigned int counter = 0;
 
 void tsk_Test1(void *param){
     (void)(param); // Cast unused variable to void
+    CAN_msg_t rx_msg;
     for(;;){
+        if(CANMsgAvail(CAN1)){
+        hal_can_receive(CAN1, &rx_msg);
+        printf("ID: %d\n", rx_msg.data[0]);
+        }
+        vTaskDelay(10);
         // Print out the systemtick timer once a second
-        printf("%ld\n", (long)xTaskGetTickCount());
-        vTaskDelay(1000);
+        // printf("%ld\n", (long)xTaskGetTickCount());
+        // vTaskDelay(1000);
     }
 }
 
