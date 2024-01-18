9346754711ecfc92067e039fec1a65fe2a345bb0
Jacob Chisholm <jacobchisholm1010@gmail.com>
2024-01-18 14:51:08 -0500

    Update testTasks.c

--- Q24ECU/core/src/tasks/testTasks.c
+++ Q24ECU/core/src/tasks/testTasks.c
@@ -26,40 +26,10 @@ void tsk_Test1(void *param){
         incoming = can_fetch(CAN1, 200);
         printf(">RPM:%d\n", incoming.data[0]);
         incoming.len = 1;
-        //incoming.data[0] = 232;
         incoming.id = 99;
         can_send_msg(CAN1, &incoming, portMAX_DELAY);
-        //can_send_msg(CAN1, &incoming);
-        //hal_can_send(CAN1, &incoming, 2);
-        //vTaskDelay(10);
-        //incoming.id = 100;
-        //hal_can_send(CAN1, &incoming, 1);
-        //while(!hal_can_send_ready(CAN1, 2)) asm("nop");
-        //while(!hal_can_send_ready(CAN1, 1)) asm("nop");
         vTaskDelay(200);
     }
-    // // printf("Starting CAN Init..\n");
-    // int can_status = hal_can_init(CAN1, CAN_1000KBPS, true, PIN('A', 11), PIN('A', 12));
-    // hal_CAN_msg_t rx_msg, tx_msg;
-    // tx_msg.len = 0;
-    // tx_msg.data[0] = 0;
-    // tx_msg.format = STANDARD_FORMAT;
-    // tx_msg.type = DATA_FRAME;
-    // tx_msg.id = 0;
-    // // printf("CAN init Finished: %d\n", can_status);
-    // for (;;)
-    // {
-    //     tx_msg.id++;
-    //     if(tx_msg.id > 999) tx_msg.id = 0;
-    //     // printf("Sending MSG");
-    //     hal_can_send(CAN1, &tx_msg);
-    //     while(hal_can_send_ready(CAN1) == 0) vTaskDelay(10);
-    //     // printf("\n");
-    //     vTaskDelay(10);
-    //     // Print out the systemtick timer once a second
-    //     // printf("%ld\n", (long)xTaskGetTickCount());
-    //     // vTaskDelay(1000);
-    // }
 }
 
 
@@ -74,9 +44,7 @@ void tsk_BlinkLED(void *param){
         msg.id = 98;
         msg.len = 1;
         msg.data[0] = 22;
-        //hal_can_send(CAN1, &msg, 1);
         can_send_msg(CAN1, &msg, portMAX_DELAY);
-        //while(hal_can_send_ready(CAN1, 1));
         vTaskDelayUntil(&lastWakeTime, 100);
     }
 }
