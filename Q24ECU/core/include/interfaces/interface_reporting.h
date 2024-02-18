d413ce451bb08a4e319b4412212b9ebc064b21d7
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2024-02-17 19:57:51 -0500

    Create interface_reporting.h

---
+++ Q24ECU/core/include/interfaces/interface_reporting.h
@@ -0,0 +1,15 @@
+/**
+ * @file interface_reporting.h
+ * @author Jacob Chisholm (https://Jchisholm204.github.io)
+ * @brief Error reporting interface
+ * @version 0.1
+ * @date 2024-02-17
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "interfaces/interface_uart.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "taskHandlers.h"
