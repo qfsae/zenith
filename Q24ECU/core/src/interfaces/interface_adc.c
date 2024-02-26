0ec4d065834b6b8c1b02beee60094c1048e1af02
Jacob Chisholm <jacobchisholm1010@gmail.com>
2024-02-26 12:21:21 -0500

    documentation

--- Q24ECU/core/src/interfaces/interface_adc.c
+++ Q24ECU/core/src/interfaces/interface_adc.c
@@ -7,8 +7,10 @@
  * 
  * @copyright Copyright (c) 2024
  * 
- * This implementation is based off of an interrupt approach to reading the ADC.
- * TODO: Add DMA support for ADC
+ * ADC readings are automatically looped and loaded into RAM via the DMA.
+ * The number of readings done by the ADC (setup through the sequence length and config channel)
+ * must be equal to the number of transfers done by the DMA. This is done through `ADC_CHANNEL_MAX`
+ * in the `ADC_CHANNELS` enum.
  * 
  */
 
@@ -21,7 +23,16 @@
 
 uint16_t ADC_READINGS[ADC_CHANNEL_MAX];
 
-
+/**
+ * @brief OS ADC Setup Handler
+ * 
+ * This function should be called to init the ADC bus and handler interfaces
+ * This function should not be called more than once
+ * ALL CAN bus initialization should happen within this function
+ * 
+ * Called in main
+ * 
+ */
 void os_adc_setup(void) {
 
     // Zero out ADC Readings
@@ -38,14 +49,14 @@ void os_adc_setup(void) {
     gpio_set_mode(PIN_A5Vin_2, GPIO_MODE_ANALOG);
 
     // Setup DMA for ADC1
-    hal_dma_init(DMA2, DMA2_Stream0, DMA_CHANNEL_0, DMA_PRIORITY_LOW, DMA_MEM_SIZE_16, &(ADC1->DR), ADC_READINGS, 2);
+    hal_dma_init(DMA2, DMA2_Stream0, DMA_CHANNEL_0, DMA_PRIORITY_LOW, DMA_MEM_SIZE_16, &(ADC1->DR), ADC_READINGS, ADC_CHANNEL_MAX);
     // Setup ADC1
     hal_adc_init(ADC1, ADC_RESOLUTION_12_BIT);
     // Setup Channel Sequence
     hal_adc_configChannel(ADC1, PIN_A5Vin_1_ADCCH, ADC_CYCLES_480, ADC_SQ1);
     hal_adc_configChannel(ADC1, PIN_A5Vin_2_ADCCH, ADC_CYCLES_480, ADC_SQ2);
     // Set Sequence Length
-    hal_adc_set_sequence_len(ADC1, 2);
+    hal_adc_set_sequence_len(ADC1, ADC_CHANNEL_MAX);
 
     spin(9999999UL); // Wait for ADC to stabilize
 
@@ -65,23 +76,33 @@ void vTask_ADCMonitor(void *param) {
     hal_adc_startConversions(ADC1);
 
     for(;;) {
-        // Print ADC Readings
+        /**
+         * ADC Overflow Checking and Recovery
+         * This task may be shutdown or changed to replace ADC data with zeros in the future.
+         * Further Error checking should be done on the values produced by the ADC to determine
+         * possible hardware or software faults.
+         */
         if(READ_BIT(ADC1->SR, ADC_SR_OVR)) sysError_report(ADC_OVERFLOW);
         if(sysError_check(ADC_OVERFLOW) == true){
-            // Attempt to clear the overflow
+            // Attempt to clear the overflow (Stop all conversion)
             hal_adc_stopConversions(ADC1);
             hal_dma_stop(DMA2_Stream0);
+            // Wait for system to catch up
             vTaskDelay(5);
+            // Clear the Overflow Bit
             CLEAR_BIT(ADC1->SR, ADC_SR_OVR);
+            // Wait
             vTaskDelay(5);
+            // Restart the conversions
             hal_dma_start(DMA2_Stream0);
             hal_adc_startConversions(ADC1);
+            // Clear the error if the ADC has started a new conversion
             if(READ_BIT(ADC1->SR, ADC_SR_STRT)){
                 sysError_clear(ADC_OVERFLOW);
             }
         }
-        // Trigger a new round of conversions
-        // SET_BIT(ADC1->CR2, ADC_CR2_SWSTART);
+        // This task is not designed for fault detection, only software error recovery.
+        // Use long delay with high task priority to ensure that the task runs consistently
         vTaskDelay(500);
     }
 }
